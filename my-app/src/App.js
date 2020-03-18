e63cceb9dde0b73e14e709be9f2eb537a58ae2ff
Ian Fragoso <ianfragoso@Ians-MacBook-Pro.local>
2020-03-17 17:55:21 -0700

    refactored to use Axios, rendering User and follower data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Component Lifecycle Methods 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React Github User Card
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import axios from "axios";
+import UserCard from './Components/UserCard';
+import FollowerCard from './Components/FollowerCard';
+
+class App extends React.Component {
+  state = {
+    user: [],
+    followers:[],
+  };
+
+ 
+
+  componentDidMount() {
+    axios
+      .get(`https://api.github.com/users/ianpaulfo`)
+      .then(res => {
+        this.setState({
+          user: [res.data]
+        });
+        console.log("Axios Call User Data:", res.data);
+      })
+        .catch(err => console.log(err));
+
+    axios
+     .get(`https://api.github.com/users/ianpaulfo/followers`)
+
+      .then(res => {
+        this.setState({ followers: res.data });
+        console.log("Axios Call followers Data:", res.data);
+      })
+      .catch(err => console.log(err));
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>GitHub User</h1>
+        {this.state.user.map(item => (
+          <UserCard
+          key={item.id}
+          name={item.name}
+          login={item.login}
+          bio={item.bio}
+          img={item.avatar_url}
+          url={item.html_url}
+          location={item.location}
+       />
+       ))}
+        <h2>Followers:</h2>
+        <div className="followers">
+        {this.state.followers.map(item => (
+            <FollowerCard
+            key={item.id}
+            login={item.login}
+            img={item.avatar_url}
+            url={item.html_url}
+            />
+          ))}
+        </div>      
+      </div>
+    );
+  }
 }
 
 export default App;
